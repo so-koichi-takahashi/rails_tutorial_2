8720d90cf77c7bd7e16425000edfc51be4b14150
高橋仰一 <koichi.takahashi@63-23413.local>
2021-06-03 12:39:43 +0900

    for connect heroke

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    def hello
+        render html: "Hello! I am Koichi!¡"
+    end
+
+    def goodbye
+        render html: "byebye!"
+    end
 end
